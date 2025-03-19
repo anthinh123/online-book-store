5273814bcc0b5993450614f215787e9ac13a1567
thinh <anvanthinh@gmail.com>
2025-03-19 23:58:21 +0700

    config postgres

--- src/main/java/com/thinh/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/com/thinh/onlinebookstore/OnlineBookStoreApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OnlinebookstoreApplication {
+public class OnlineBookStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OnlinebookstoreApplication.class, args);
+		SpringApplication.run(OnlineBookStoreApplication.class, args);
 	}
 
 }
--- src/main/java/com/thinh/onlinebookstore/book/service/BookService.java
+++ src/main/java/com/thinh/onlinebookstore/book/service/BookService.java
@@ -3,12 +3,14 @@ package com.thinh.onlinebookstore.book.service;
 import com.thinh.onlinebookstore.book.dto.BookDto;
 import com.thinh.onlinebookstore.book.mapper.BookMapper;
 import com.thinh.onlinebookstore.book.repository.BookRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@AllArgsConstructor
 public class BookService {
 
     private BookRepository bookRepository;
